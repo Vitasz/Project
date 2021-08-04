b81e20daf10f0f53b8dc32b6fdf93f46f68069d4
Vitasz <79321565+Vitasz@users.noreply.github.com>
2021-08-04 22:06:44 +0300

    Add Grid

---
+++ New Unity Project/Assets/Scripts/GameControlls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameControlls : MonoBehaviour
+{
+    public void ClickOnGrid(int X, int Y)
+    {
+        Debug.Log("Coordinates: X: " + Convert.ToString(X) + " Y: " + Convert.ToString(Y));
+    }
+}
--- New Unity Project/Assets/Scripts/GridFunc.cs
+++ New Unity Project/Assets/Scripts/GridFunc.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GridFunc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int SizeX, SizeY, SizeCell;
+    public GameControlls GameController;
+    public Material MaterialForLines;
+    public BoxCollider2D Collider;
+    private readonly float _linesWidth = 0.3f;
+    private void Start()
     {
-        
+        void CreateLine(Vector3[] Positions)
+        {
+            GameObject NewLineGrid = new GameObject();
+            NewLineGrid.transform.parent = transform;
+            LineRenderer NewLineGridLR = NewLineGrid.AddComponent<LineRenderer>();
+            NewLineGridLR.startWidth = _linesWidth;
+            NewLineGridLR.endWidth = _linesWidth;
+            NewLineGridLR.startColor = Color.black;
+            NewLineGridLR.endColor = Color.black;
+            NewLineGridLR.material = MaterialForLines;
+            NewLineGridLR.SetPositions(Positions);
+        }
+        for (int i = 0; i <= SizeX; i++)
+        {
+            Vector3[] Positions = new Vector3[] { new Vector3((float)i * SizeCell - (float)SizeX / 2 * SizeCell, (float)SizeY / 2 * SizeCell),
+               new Vector3((float)i * SizeCell - (float)SizeX / 2 * SizeCell, -(float)SizeY / 2 * SizeCell)
+               };
+            CreateLine(Positions);
+        }
+        for (int i = 0; i <= SizeY; i++)
+        {
+            Vector3[] Positions = new Vector3[] { new Vector3((float)SizeX / 2 * SizeCell, (float)i * SizeCell - (float)SizeY / 2 * SizeCell),
+               new Vector3(-(float)SizeX / 2 * SizeCell, (float)i * SizeCell - (float)SizeY / 2 * SizeCell)
+               };
+            CreateLine(Positions);
+        }
+        Collider.size = new Vector2(SizeX * SizeCell * 2, SizeY * SizeCell * 2);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (SizeX % 2 == 0) worldPosition.x -= SizeCell / 2;
+        if (SizeY % 2 == 0) worldPosition.y -= SizeCell / 2;
+        GameController.ClickOnGrid(Mathf.RoundToInt(worldPosition.x / SizeCell) + SizeX/2, Mathf.RoundToInt(worldPosition.y / SizeCell) + SizeY / 2);
     }
 }
